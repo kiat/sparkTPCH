9028f6fd43d37f57f371d93d997c75b745c66c4d
kiat <kiat@rice.edu>
2017-06-01 17:20:15 -0500

    minor changes

--- src/main/java/edu/rice/exp/spark_exp/AggregatePartIDsFromCustomer_Dataset.java
+++ src/main/java/edu/rice/exp/spark_exp/AggregatePartIDsFromCustomer_Dataset.java
@@ -3,31 +3,21 @@ package edu.rice.exp.spark_exp;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.MapFunction;
-import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.api.java.UDF1;
 
-import scala.Tuple2;
-import scala.collection.Seq;
 import edu.rice.dmodel.Customer;
 import edu.rice.dmodel.LineItem;
 import edu.rice.dmodel.Order;
-import edu.rice.dmodel.Part;
-import edu.rice.dmodel.Supplier;
 import edu.rice.dmodel.SupplierCustomerPartID;
 import edu.rice.dmodel.SupplierData;
 import edu.rice.dmodel.TupleCustomerNameLineItem;
@@ -35,7 +25,7 @@ import edu.rice.generate_data.DataGenerator;
 
 
 //col("...") is preferable to df.col("...")
-import static  org.apache.spark.sql.functions.*;
+//import static  org.apache.spark.sql.functions.*;
 
 
 
@@ -49,7 +39,7 @@ public class AggregatePartIDsFromCustomer_Dataset {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		long startTime = 0;
 		double elapsedTotalTime = 0;
-		int NUMBER_OF_COPIES =1;
+		int NUMBER_OF_COPIES =2;
 		
 		if(args.length>0)
 		NUMBER_OF_COPIES = Integer.parseInt(args[0]);
@@ -80,7 +70,7 @@ public class AggregatePartIDsFromCustomer_Dataset {
 		    );
 		    
 		    
-//		    customerDS.show();
+		    customerDS.show();
 		  
 		    // Copy the same data multiple times to make it big data 
 		    // Original number is 15K 
@@ -92,15 +82,15 @@ public class AggregatePartIDsFromCustomer_Dataset {
 		    customerDS.cache();
 		    System.out.println("Number of Customers in Dataset: "+customerDS.count());
 		    
-//			// fore to do the garbage collection 
-//			System.gc();
-//			
-//	       // try to sleep for 5 seconds to be sure that all other tasks are done 
-//			try {
-//				Thread.sleep(5000);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
+			// fore to do the garbage collection 
+			System.gc();
+			
+	       // try to sleep for 5 seconds to be sure that all other tasks are done 
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 	
 		  
 			// Now is data loaded in RDD, ready for the experiment
@@ -124,9 +114,6 @@ public class AggregatePartIDsFromCustomer_Dataset {
 					return returnList.iterator();
 				}}, tupleCustomerNameLineItem);
 		
-			
-			
-//			customerNameLineItem.show();
 		    System.out.println("Number of TupleCustomerNameLineItem in Dataset: " + customerNameLineItem.count());
 		    
 		    Dataset<SupplierCustomerPartID>  supplierCustomerPartID_DS=customerNameLineItem.map(new MapFunction<TupleCustomerNameLineItem,SupplierCustomerPartID>(){
@@ -159,116 +146,6 @@ public class AggregatePartIDsFromCustomer_Dataset {
 		    
 		    System.out.println(dataSet1.schema());
 		    
-//		    Dataset<SupplierData>  supplierData_DS= dataSet1.map(new MapFunction<Row, SupplierData>() {
-//
-//				private static final long serialVersionUID = -5684258513081782258L;
-//
-//				@Override
-//				public SupplierData call(Row arg0) throws Exception {
-//					SupplierData returnResult=new SupplierData();
-//					String customerName=arg0.getAs("customerName");
-//
-//					Integer[] partIDs= arg0.getAs("partID");
-//					String[] supplierNames= arg0.getAs("supplierName");
-//					
-//					
-//					
-//					
-//					returnResult.setCustomerName(customerName);
-//					
-//					
-//					
-//					return returnResult;
-//				}
-//		    	
-//		    }, supplierData_encoder );
-
-		    
-//		    dataSet1.toJavaRDD().coalesce(1,true).saveAsTextFile("output");
-		    
-		    
-//		    Dataset<SupplierData>  supplierData_DS= dataSet1.map(new MapFunction<Row, SupplierData>() {
-//				private static final long serialVersionUID = -5684258513081782258L;
-//				@Override
-//				public SupplierData call(Row arg0) throws Exception {
-//					SupplierData returnResult=new SupplierData();
-//					 Map<String, List<Integer>> tmp= new  HashMap<String, List<Integer>>();
-//					 String key=(String)arg0.get(0);
-//					 List<Integer> tmpList= (List<Integer>) arg0.get(1);
-//					 tmp.put(key, tmpList);
-//					return returnResult;
-//				}
-//		    }, supplierData_encoder);
-//		    
-//		    supplierData_DS.show();
-
-//		    Dataset<SupplierCustomerPartID>  supplierCustomerPartID_DS=customerNameLineItem.map(new Function1<TupleCustomerNameLineItem,U>, supplierCustomerPartID_encoder)() {
-//
-//				@Override
-//				public Iterator<SupplierCustomerPartID> call(TupleCustomerNameLineItem arg0) throws Exception {
-//
-//					SupplierCustomerPartID returnValue=new SupplierCustomerPartID(); 
-//					
-//					return returnValue;
-//				}}, supplierCustomerPartID_encoder);
-
-		    
-//		
-//		JavaRDD< Tuple2<String,  LineItem>>  soldLineItems = customerRDD.flatMap(new FlatMapFunction<Customer, Tuple2<String,  LineItem>>() {
-//
-//			private static final long serialVersionUID = -7539917700784174380L;
-//
-//			@Override
-//			public Iterator<Tuple2<String, LineItem>> call(Customer customer) throws Exception {
-//				List<Order> orders= customer.getOrders();
-//				
-//				List<Tuple2<String, LineItem>> returnList = new ArrayList<Tuple2<String, LineItem>>();
-//				for (Order order : orders) {
-//					List<LineItem> lineItems= order.getLineItems();
-//					for (LineItem  lineItem : lineItems) {
-//						returnList.add(new Tuple2<String, LineItem>(customer.getName(), lineItem));
-//					}
-//				}
-//				return returnList.iterator();
-//			}	
-//		});
-//		
-//		
-//		JavaPairRDD<String,  Tuple2<String,  Integer>>  soldPartIDs =soldLineItems
-//				.mapToPair(w ->  new Tuple2 <String, Tuple2<String,  Integer>>(w._2.getSupplier().getName() , new Tuple2<String,  Integer>(w._1, w._2.getPart().getPartID()))); 
-//		
-//		
-//		 // Now, we need to aggregate the results 
-//		// aggregateByKey needs 3 parameters:
-//		// 1. zero initializations, 
-//		// 2. a function to add data to a supplierData object and 
-//		// 3. a function to merge two supplierData objects. 
-//		JavaPairRDD<String, SupplierData> result = soldPartIDs.aggregateByKey(new SupplierData(), new Function2<SupplierData, Tuple2<String,  Integer>, SupplierData>(){
-//
-//			private static final long serialVersionUID = 7295222894776950599L;
-//
-//			@Override
-//			public SupplierData call(SupplierData suppData, Tuple2<String, Integer> tuple) throws Exception {
-//				suppData.addCustomer(tuple._1, tuple._2);
-//				return suppData;
-//			}
-//			
-//		}, new Function2<SupplierData, SupplierData, SupplierData>(){
-//			private static final long serialVersionUID = -1503342516335901464L;
-//
-//			@Override
-//			public SupplierData call(SupplierData suppData1, SupplierData suppData2) throws Exception {
-//				
-//				 Map<String, List<Integer>> tmp=suppData1.getSoldPartIDs();
-//				 tmp.putAll(suppData2.getSoldPartIDs());
-//				 suppData1.setSoldPartIDs(tmp);
-//				
-//				return suppData1;
-//			}});
-//		
-//
-//		result.saveAsTextFile("output");
-		
 		// Stop the timer
 		elapsedTotalTime += (System.nanoTime() - startTime) / 1000000000.0;
 		
