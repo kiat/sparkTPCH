f60c341ae758bbc69732d7737c5a840475dc4362
kiat <kiat@rice.edu>
2017-06-21 16:46:20 -0500

    Kryo Serialization

---
+++ src/main/java/edu/rice/dmodel/MyKryoRegistrator.java
@@ -0,0 +1,103 @@
+package edu.rice.dmodel;
+
+import java.io.Serializable;
+import java.lang.reflect.Array;
+
+import javax.annotation.Nonnull;
+
+import org.apache.spark.serializer.KryoRegistrator;
+
+import com.esotericsoftware.kryo.Kryo;
+
+// Based on http://lordjoesoftware.blogspot.com/2015/02/using-kryoserializer-in-spark.html
+public class MyKryoRegistrator implements KryoRegistrator, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7700960882306273668L;
+
+	public MyKryoRegistrator() {
+	}
+
+	/**
+	 * register a class indicated by name
+	 *
+	 * @param kryo
+	 * @param s
+	 *            name of a class - might not exist
+	 * @param handled
+	 *            Set of classes already handles
+	 */
+	protected void doRegistration(@Nonnull Kryo kryo, @Nonnull String s) {
+		Class c;
+		try {
+			c = Class.forName(s);
+			doRegistration(kryo, c);
+		} catch (ClassNotFoundException e) {
+			return;
+		}
+	}
+
+	/**
+	 * register a class
+	 *
+	 * @param kryo
+	 * @param s
+	 *            name of a class - might not exist
+	 * @param handled
+	 *            Set of classes already handles
+	 */
+	protected void doRegistration(final Kryo kryo, final Class pC) {
+		if (kryo != null) {
+			kryo.register(pC);
+			// also register arrays of that class
+			Class arrayType = Array.newInstance(pC, 0).getClass();
+			kryo.register(arrayType);
+		}
+	}
+
+	/**
+	 * do the real work of registering all classes
+	 * 
+	 * @param kryo
+	 */
+	@Override
+	public void registerClasses(@Nonnull Kryo kryo) {
+		kryo.register(Object[].class);
+		kryo.register(scala.Tuple2[].class);
+
+		kryo.register(java.util.List.class);
+		kryo.register(java.util.ArrayList.class);
+		kryo.register(java.util.HashMap.class);
+
+		// register my classes. 
+		kryo.register(edu.rice.dmodel.Part.class);
+		kryo.register(edu.rice.dmodel.Supplier.class);
+		kryo.register(edu.rice.dmodel.Order.class);
+		kryo.register(edu.rice.dmodel.LineItem.class);
+		kryo.register(edu.rice.dmodel.Customer.class);
+		kryo.register(edu.rice.dmodel.SupplierData.class);
+		
+
+		doRegistration(kryo, "scala.collection.mutable.WrappedArray$ofRef");
+		// doRegistration(kryo,
+		// "org.systemsbiology.xtandem.scoring.VariableStatistics");
+		// doRegistration(kryo,
+		// "org.systemsbiology.xtandem.scoring.SpectralPeakUsage$PeakUsage");
+		// // and many more similar nines
+
+		// kryo.register(Part.class, new FieldSerializer(kryo, Part.class));
+		// kryo.register(Supplier.class, new FieldSerializer(kryo,
+		// Supplier.class));
+		// kryo.register(Order.class, new FieldSerializer(kryo, Order.class));
+		// kryo.register(LineItem.class, new FieldSerializer(kryo,
+		// LineItem.class));
+		// kryo.register(Customer.class, new FieldSerializer(kryo,
+		// Customer.class));
+		// kryo.register(SupplierData.class, new FieldSerializer(kryo,
+		// SupplierData.class));
+
+	}
+
+};
--- src/main/java/edu/rice/exp/spark_exp/AggregatePartIDsFromCustomer_RDD.java
+++ src/main/java/edu/rice/exp/spark_exp/AggregatePartIDsFromCustomer_RDD.java
@@ -13,113 +13,131 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.serializer.KryoRegistrator;
 
 import scala.Tuple2;
+
+import com.esotericsoftware.kryo.Kryo;
+
 import edu.rice.dmodel.Customer;
 import edu.rice.dmodel.LineItem;
 import edu.rice.dmodel.Order;
+import edu.rice.dmodel.Part;
+import edu.rice.dmodel.Supplier;
 import edu.rice.dmodel.SupplierData;
 import edu.rice.generate_data.DataGenerator;
 
 public class AggregatePartIDsFromCustomer_RDD {
-	
-	
-	private static JavaSparkContext sc;
 
+	public static class MyKryoRegistrator implements KryoRegistrator {
+		@Override
+		public void registerClasses(Kryo kryo) {
+			kryo.register(Part.class);
+			kryo.register(Supplier.class);
+			kryo.register(Order.class);
+			kryo.register(LineItem.class);
+			kryo.register(Customer.class);
+			kryo.register(SupplierData.class);
+			kryo.register(Tuple2.class);
+
+		}
+	}
+
+	private static JavaSparkContext sc;
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		long startTime = 0;
 		double elapsedTotalTime = 0;
-		int NUMBER_OF_COPIES =0;
-		String fileScale="0.1";
+		int NUMBER_OF_COPIES = 0;
+		String fileScale = "0.1";
 
-		
-		if(args.length>0)
-		NUMBER_OF_COPIES = Integer.parseInt(args[0]);
+		if (args.length > 0)
+			NUMBER_OF_COPIES = Integer.parseInt(args[0]);
 
 		PropertyConfigurator.configure("log4j.properties");
 
-		SparkConf sparkConf = new SparkConf()
-		        .setAppName("ComplexObjectManipulation")
-				.setMaster("local[*]")
-				.set("spark.executor.memory", "32g")
-				;
+		SparkConf conf = new SparkConf().setAppName("ComplexObjectManipulation").setMaster("local[*]").set("spark.executor.memory", "32g");
 
-		sc = new JavaSparkContext(sparkConf);
-		
+		conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+		conf.set("spark.kryoserializer.buffer.mb","64");
+		conf.set("spark.kryo.registrationRequired", "true");
+//		conf.set("spark.kryo.registrator", MyKryoRegistrator.class.getName());
+		conf.set("spark.kryo.registrator","edu.rice.dmodel.MyKryoRegistrator");
+
+
+		sc = new JavaSparkContext(conf);
+
 		if (args.length > 1)
 			fileScale = args[1];
-		
+
 		JavaRDD<Customer> customerRDD = sc.parallelize(DataGenerator.generateData(fileScale));
-		
-		// Copy the same data multiple times to make it big data 
+
+		// Copy the same data multiple times to make it big data
 		for (int i = 0; i < NUMBER_OF_COPIES; i++) {
 			customerRDD = customerRDD.union(customerRDD);
 		}
-		
+
 		customerRDD.cache();
 
-		// force spark to do the job and load data into RDD 
-		
-		long numberOfCustomers=customerRDD.count();
+		// force spark to do the job and load data into RDD
+
+		long numberOfCustomers = customerRDD.count();
 		System.out.println("Number of Customer: " + numberOfCustomers);
-		
-		
-//		// fore to do the garbage collection 
-//		System.gc();
-//		
-//
-//		// try to sleep for 5 seconds to be sure that all other tasks are done 
-//		for (int i = 0; i < 5; i++) {
-//			try {
-//				Thread.sleep(1000);
-//				System.out.println("Sleep for 1 sec ... ");
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}	
-//		}
-		
+
+		// // fore to do the garbage collection
+		// System.gc();
+		//
+		//
+		// // try to sleep for 5 seconds to be sure that all other tasks are
+		// done
+		// for (int i = 0; i < 5; i++) {
+		// try {
+		// Thread.sleep(1000);
+		// System.out.println("Sleep for 1 sec ... ");
+		// } catch (InterruptedException e) {
+		// e.printStackTrace();
+		// }
+		// }
+
 		System.out.println("Data is ready to use. ");
-		
+
 		// #############################################
 		// #############################################
-		// #########     MAIN Experiment   #############
+		// ######### MAIN Experiment #############
 		// #############################################
 		// #############################################
-		
+
 		// Now is data loaded in RDD, ready for the experiment
 		// Start the timer
 		startTime = System.nanoTime();
-		
-		JavaRDD<Tuple2<String,  Tuple2<String, Integer>>>  soldLineItems2 = customerRDD.flatMap(new FlatMapFunction<Customer, Tuple2<String,  Tuple2<String, Integer>>>() {
+
+		JavaRDD<Tuple2<String, Tuple2<String, Integer>>> soldLineItems2 = customerRDD.flatMap(new FlatMapFunction<Customer, Tuple2<String, Tuple2<String, Integer>>>() {
 
 			private static final long serialVersionUID = -7539917700784174380L;
 
 			@Override
 			public Iterator<Tuple2<String, Tuple2<String, Integer>>> call(Customer customer) throws Exception {
-				List<Order> orders= customer.getOrders();
+				List<Order> orders = customer.getOrders();
 				List<Tuple2<String, Tuple2<String, Integer>>> returnList = new ArrayList<Tuple2<String, Tuple2<String, Integer>>>();
 				for (Order order : orders) {
-					List<LineItem> lineItems= order.getLineItems();
-					for (LineItem  lineItem : lineItems) {
-						Tuple2<String, Integer> supplierPartID=new Tuple2<String, Integer>(customer.getName(), lineItem.getPart().getPartID() );
-						returnList.add(new Tuple2<String, Tuple2<String, Integer>>(lineItem.getSupplier().getName() , supplierPartID));
+					List<LineItem> lineItems = order.getLineItems();
+					for (LineItem lineItem : lineItems) {
+						Tuple2<String, Integer> supplierPartID = new Tuple2<String, Integer>(customer.getName(), lineItem.getPart().getPartID());
+						returnList.add(new Tuple2<String, Tuple2<String, Integer>>(lineItem.getSupplier().getName(), supplierPartID));
 					}
 				}
 				return returnList.iterator();
-			}	
+			}
 		});
-		
 
-		JavaPairRDD<String,  Tuple2<String,  Integer>>  soldPartIDs =soldLineItems2
-		.mapToPair(w ->  new Tuple2 <String, Tuple2<String,  Integer>>(w._1 , w._2)); 
-		
-		 // Now, we need to aggregate the results 
+		JavaPairRDD<String, Tuple2<String, Integer>> soldPartIDs = soldLineItems2.mapToPair(w -> new Tuple2<String, Tuple2<String, Integer>>(w._1, w._2));
+
+		// Now, we need to aggregate the results
 		// aggregateByKey needs 3 parameters:
-		// 1. zero initializations, 
-		// 2. a function to add data to a supplierData object and 
-		// 3. a function to merge two supplierData objects. 
-		JavaPairRDD<String, SupplierData> result = soldPartIDs.aggregateByKey(new SupplierData(), new Function2<SupplierData, Tuple2<String,  Integer>, SupplierData>(){
+		// 1. zero initializations,
+		// 2. a function to add data to a supplierData object and
+		// 3. a function to merge two supplierData objects.
+		JavaPairRDD<String, SupplierData> result = soldPartIDs.aggregateByKey(new SupplierData(), new Function2<SupplierData, Tuple2<String, Integer>, SupplierData>() {
 
 			private static final long serialVersionUID = 7295222894776950599L;
 
@@ -128,8 +146,8 @@ public class AggregatePartIDsFromCustomer_RDD {
 				suppData.addCustomer(tuple._1, tuple._2);
 				return suppData;
 			}
-			
-		}, new Function2<SupplierData, SupplierData, SupplierData>(){
+
+		}, new Function2<SupplierData, SupplierData, SupplierData>() {
 			private static final long serialVersionUID = -1503342516335901464L;
 
 			@Override
@@ -137,17 +155,17 @@ public class AggregatePartIDsFromCustomer_RDD {
 				// merge the two HashMaps inside the SupplierData
 				suppData1.merge(suppData2);
 				return suppData1;
-			}});
-		
+			}
+		});
 
-		// At the end, what we need is a vector of SupplierData 
+		// At the end, what we need is a vector of SupplierData
 		JavaRDD<SupplierData> resultFinal = result.map(x -> x._2);
-		
-		System.out.println("Final Result Count:"+resultFinal.count());
-		
+
+		System.out.println("Final Result Count:" + resultFinal.count());
+
 		// Stop the timer
 		elapsedTotalTime += (System.nanoTime() - startTime) / 1000000000.0;
-		
-		System.out.println(numberOfCustomers+"#" +String.format("%.9f", elapsedTotalTime));
+
+		System.out.println(numberOfCustomers + "#" + String.format("%.9f", elapsedTotalTime));
 	}
 }
